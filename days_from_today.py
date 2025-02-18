49449dc78928dcdd6113d9e8ebb00c6f652aa596
yarqui <y.pelykh@gmail.com>
2025-02-18 15:25:12 -0500

    add days_from_today function

---
+++ days_from_today.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+def get_days_from_today(date: str) -> int:
+    try:
+        if type(date) is not str:
+            raise TypeError('Date should be an integer')
+        
+        current_date = datetime.today()
+        ref_date = datetime.strptime(date, '%Y-%m-%d')
+        
+        return (current_date - ref_date).days
+        
+    except Exception as err:
+        print(f'{type(err)}: {err}')
+    
+print(get_days_from_today(bool))
+print(get_days_from_today('2027-02-02'))
+print(get_days_from_today('2027'))
