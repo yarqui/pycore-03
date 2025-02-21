9d7e20176b8caec206586a63f0a921a9977fc855
yarqui <y.pelykh@gmail.com>
2025-02-20 22:27:53 -0500

    add get_numbers_ticket func

---
+++ get_numbers_ticket.py
@@ -0,0 +1,13 @@
+import random
+
+def get_numbers_ticket(min, max, quantity):
+    l = list()
+    if min<1 or max>1000:
+        return l
+    
+    nums = range(min,max)
+    l = sorted(random.sample(nums, k=quantity))
+    return l
+
+winners = get_numbers_ticket(1,100, 5)
+print(winners)
