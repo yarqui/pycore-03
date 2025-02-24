c298236a88e013c1870bd38e80a32a427b8aa71f
yarqui <y.pelykh@gmail.com>
2025-02-24 11:25:57 -0500

    add check for quantity not exceeding possible number of tickets between min & max

--- get_numbers_ticket.py
+++ get_numbers_ticket.py
@@ -2,12 +2,14 @@ import random
 
 def get_numbers_ticket(min, max, quantity):
     l = list()
-    if min<1 or max>1000:
+    if min<1 or max>1000 or ((max - min) < quantity):
         return l
     
+
     nums = range(min,max)
     l = sorted(random.sample(nums, k=quantity))
     return l
 
-winners = get_numbers_ticket(1,100, 5)
+# winners = get_numbers_ticket(1,100, 5)
+winners = get_numbers_ticket(10, 12, 11)
 print(winners)
